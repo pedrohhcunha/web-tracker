c5b3d15c69c233484f7a6338edc1afa7aaf057d6
Pedrohhcunha <pedro.cunha@ixcsoft.com.br>
2023-02-24 19:08:39 -0300

    feat(client): adjust link on client.js

--- src/public/client.js
+++ src/public/client.js
@@ -309,7 +309,7 @@ function identifyElement(event) {
  * It sends browser events to the server, each 2 seconds.
  */
 function init() {
-  const socket = io('https://web-tracker.herokuapp.com:3333');
+  const socket = io('https://web-tracker-yucl.onrender.com');
 
   socket.on('connect', async () => {
 
