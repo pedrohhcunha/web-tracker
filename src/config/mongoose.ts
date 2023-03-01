a7f1c1dd6bdbc64ffa66d14da1db959d889f5628
Pedrohhcunha <pedro.cunha@ixcsoft.com.br>
2023-03-01 19:00:54 -0300

    fix(server): remove mongoose warn

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,10 +1,10 @@
-import { connect } from 'mongoose';
+import { connect, set } from 'mongoose';
 
 export async function connectMongoose() {
     try {
         const MONGO_URI = process.env.MONGO_URI || '';
+        set("strictQuery", true);
         await connect(MONGO_URI);
-        console.log('Connected to MongoDB');
     } catch (err) {
         console.error(err);
     }
